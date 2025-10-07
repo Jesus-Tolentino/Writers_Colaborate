30d45a6e3abb6402bba54671b3384750e69a05d3
Jesus Tolentino <jesusmonascatolentino58@gmail.com>
2025-10-06 19:16:31 -0500

    Creación del espacio de trabajo.

---
+++ src/main/java/pe/edu/upc/writers_colaborate/WritersColaborateApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.writers_colaborate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritersColaborateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WritersColaborateApplication.class, args);
+    }
+
+}
